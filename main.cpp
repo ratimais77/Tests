08a453549bb86403b88552798c77283181fd22af
rati maisuradze <rmai77@yahoo.com>
2026-06-05 12:45:02 -0700

    Joined

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 base1
+
+base1 12
+=======
+=======
 base1 123
 bati ammended this
+rati ammended this
 what about this line being so???
 tike ammended this
+mike ammended this
 branch1-1
 
 
@@ -19,6 +25,30 @@ today I will be doing lots aof unnecesary work
 
 today also I will be doing lots aof unnecesary work
 
+<<<<<<< HEAD
+=======
+branch1 work
+
+branch2 today
 
 
-rebase 1
+squashed 1
+squashed 2
+squashed 3
+<<<<<<< HEAD
+>>>>>>> a560874 (ZZZ)
+=======
+
+ZORO
+<<<<<<< HEAD
+>>>>>>> 2aa9439 (ZORO1)
+=======
+
+ASTER
+<<<<<<< HEAD
+>>>>>>> c000aec (ASTER 1)
+=======
+
+
+MIZIZI
+>>>>>>> f09274d (Mizizi)
